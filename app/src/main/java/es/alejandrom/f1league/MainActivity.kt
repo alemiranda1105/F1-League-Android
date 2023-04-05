6e0b3e6425cc1b2ac565ce8fafd88006e88d8f0c
Alejandro Miranda <amiranda110500@gmail.com>
2023-04-05 11:43:08 +0100

    First commit

---
+++ app/src/main/java/es/alejandrom/f1league/MainActivity.kt
@@ -0,0 +1,43 @@
+package es.alejandrom.f1league
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.alejandrom.f1league.ui.theme.F1LeagueTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            F1LeagueTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    F1LeagueTheme {
+        Greeting("Android")
+    }
+}
